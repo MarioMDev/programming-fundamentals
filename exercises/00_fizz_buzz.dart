871b734e9a244504ff0b74921d55de53be9c4184
Mario Monzón <mario.monzon@bitboxonline.com>
2025-12-30 15:47:44 +0000

    Fizz Buzz Exercise

---
+++ exercises/00_fizz_buzz.dart
@@ -0,0 +1,26 @@
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
+void main() {
+  fizzBuzzV1();
+}
+
+void fizzBuzzV1(){
+  for (int i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+      print("fizz");
+    } else if (i % 5 == 0) {
+      print("buzz");
+    } else if ((i % 3 == 0) && (i % 5 == 0)) {
+      print("fizz buzz");
+    } else {
+      print(i);
+    }
+  }
+}
